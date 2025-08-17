ce8d91810ebb50a457fb0ad7af23562ec52ccc38
nimisha <agnihotrinimisha296@gmail.com>
2025-08-17 23:52:07 +0530

    worked on background

---
+++ portfolio/src/components/StarBackground.jsx
@@ -0,0 +1,12 @@
+import {useState} from "react";
+
+export const StarBackground=()=>{
+    const [stars,setStars]=useState([]);
+
+    const generateStarts =()=>{
+        const numberOfStars=Math.floor(
+            (window.innerWidth * window.innerHeight / 10000);
+            
+    }
+    return <div></div>;
+}
