4624dfb42f6b9c8f8b56a98f8910f4f5ef67c7dd
nimisha <agnihotrinimisha296@gmail.com>
2025-08-22 14:55:50 +0530

    started skills section

---
+++ portfolio/src/components/SkillsSection.jsx
@@ -0,0 +1,29 @@
+
+
+const skills=[
+    //frontend
+    {name:"HTML/CSS", level: 95, category:"frontend"},
+    {name:"javascript", level: 90, category:"frontend"},
+    {name:"react", level: 95, category:"frontend"},
+    {name:"typescript", level: 85, category:"frontend"},
+    {name:"tailwind css", level: 90, category:"frontend"},
+    {name:"next.js", level: 80, category:"frontend"},
+
+    //backend
+    {name:"node.js", level: 95, category:"backend"},
+    {name:"express", level: 95, category:"backend"},
+    {name:"mongo", level: 95, category:"backend"},
+    {name:"sql", level: 95, category:"backend"},
+    {name:"", level: 95, category:"backend"},
+
+    //tools
+    {name:"HTML/CSS", level: 95, category:"frontend"},
+    {name:"HTML/CSS", level: 95, category:"frontend"},
+    {name:"HTML/CSS", level: 95, category:"frontend"},
+    {name:"HTML/CSS", level: 95, category:"frontend"},
+];
+
+
+export const SkillsSection=()=>{
+    return <section></section>;
+};
