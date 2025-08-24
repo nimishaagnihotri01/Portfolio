eca985df3309976671e24cd6c26729f78c3e6b1a
nimisha <agnihotrinimisha296@gmail.com>
2025-08-24 12:05:23 +0530

    cleaning up components

--- portfolio/src/components/HeroSection.jsx
+++ portfolio/src/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 import { ArrowDown } from "lucide-react"
 
 export const HeroSection=()=>{
-    return <section id="hero" className="relative min-h-screen flex flex-col items-center justify-center px-4">
+    return <section id="home" className="relative min-h-screen flex flex-col items-center justify-center px-4">
         <div className="container max-w-4xl mx-auto text-center z-10">
             <div className="space-y-6">
                 <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Menu, X } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const navItems=[
-  {name:"Home", href: "#hero"},
+  {name:"Home", href: "#home"},
   {name:"About", href: "#about"},
   {name:"Skills", href: "#skills"},
   {name:"Projects", href: "#projects"},
@@ -27,7 +27,7 @@ export const Navbar = () => {
     )}
     >
       <div className="container flex items-center justify-between">
-          <a className="text-xl font-bold text-primary flex items-center" href="#hero">
+          <a className="text-xl font-bold text-primary flex items-center" href="#home">
             <span className="relative z-10">
               <span className="text-glow text-foreground">Nimisha's</span> portfolio
             </span>
